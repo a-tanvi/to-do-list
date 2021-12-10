ca0e3d1b0247f6a3d6ea9121d13a2d4c26f4d2ba
Ankush Dua <ankushduacodes@gmail.com>
2021-12-10 23:34:32 +0530

    added feature to add new todos to local storage... rendering them back to page is left..

--- app.js
+++ app.js
@@ -20,7 +20,26 @@ function addTodo(event) {
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
-
+    // new code here
+    const newTodoJson = {
+        id: Math.floor(Math.random() * 10000) + 10000,
+        todoData: newTodo.innerText,
+        classList: 'todo-item',
+    }
+    const {localStorage} = window;
+    const prevTodos = JSON.parse(localStorage.getItem('todos'));
+    if (!prevTodos) {
+        const todos = [
+            {
+                todo: newTodoJson,
+            }
+        ]
+        localStorage.setItem('todos', JSON.stringify(todos));
+    } else {
+        prevTodos.push({todo: newTodoJson});
+        localStorage.setItem('todos', JSON.stringify(prevTodos));
+    }
+    // new code ends here
     //bringing li inside div
     todoDiv.appendChild(newTodo);
 
@@ -39,42 +58,34 @@ function addTodo(event) {
     //append to list
     todoList.appendChild(todoDiv);
 
-    // clear todo 
+    // clear todo
     todoInput.value = "";
-
-
 }
 
 function deleteCheck(e) {
     // e.target is used to Get the element that triggered a specific event
-
     const item = e.target;
 
     // Delete todo
     if (item.classList[0] == "trash-btn") {
         const todo = item.parentElement;
         todo.classList.add("fall");
-        todo.addEventListener('transitionend', function() {
+        todo.addEventListener('transitionend', function () {
             todo.remove();
         })
-
     }
 
     // chechmark
-    if (item.classList[0] == "complte-btn") {
+    if (item.classList[0] === "complte-btn") {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
         filterTodo();
-
     }
-
 }
 
 function filterTodo(e) {
     const todos = todoList.childNodes;
-    todos.forEach(function(todo) {
-
-
+    todos.forEach(function (todo) {
         switch (filterOption.value) {
             case "all":
                 todo.style.display = 'flex';
@@ -82,7 +93,6 @@ function filterTodo(e) {
             case "completed":
                 if (todo.classList.contains('completed')) {
                     todo.style.display = 'flex';
-
                 } else {
                     todo.style.display = "none";
                 }
@@ -90,16 +100,18 @@ function filterTodo(e) {
             case "un-Completed":
                 if (!todo.classList.contains('completed')) {
                     todo.style.display = 'flex';
-
                 } else {
                     todo.style.display = "none";
                 }
-
-
-
-
         }
-
     });
-
 }
+
+
+function fillPreviousTodos() {
+    const {localStorage} = window;
+    const prevTodos = JSON.parse(localStorage.getItem('todos'));
+    console.log(prevTodos);
+}
+
+fillPreviousTodos();
